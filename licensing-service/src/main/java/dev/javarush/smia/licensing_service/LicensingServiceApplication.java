e31003cd4792d182fa58f05cfc1395136e4d5a92
Himanshu Pareek <himanshupareekiit01@gmail.com>
2024-08-25 16:31:58 +0000

    Reads properties from config server

--- licensing-service/src/main/java/dev/javarush/smia/licensing_service/LicensingServiceApplication.java
+++ licensing-service/src/main/java/dev/javarush/smia/licensing_service/LicensingServiceApplication.java
@@ -2,18 +2,33 @@ package dev.javarush.smia.licensing_service;
 
 import java.util.Locale;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
-public class SmiaChapter4Application {
+public class LicensingServiceApplication {
+
+	private static final Logger LOG = LoggerFactory.getLogger(LicensingServiceApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(SmiaChapter4Application.class, args);
+		SpringApplication.run(LicensingServiceApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner afterStartup(ApplicationContext context) {
+		Environment environment = context.getEnvironment();
+		return args -> {
+			LOG.info("Who are you? {}", environment.getProperty("example.property"));
+		};
 	}
 
 	@Bean
@@ -22,6 +37,7 @@ public class SmiaChapter4Application {
 		localeResolver.setDefaultLocale(Locale.US);
 		return localeResolver;
 	}
+
 	@Bean
 	public ResourceBundleMessageSource messageSource() {
 		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
@@ -29,6 +45,5 @@ public class SmiaChapter4Application {
 		messageSource.setBasenames("messages");
 		return messageSource;
 	}
-
 
 }
