0a6c29d22657d299d3022f7da2e7a4c6a69c2c7a
Himanshu Pareek <himanshupareekiit01@gmail.com>
2024-08-27 18:51:16 +0530

    Changes License record to class

--- licensing-service/src/main/java/dev/javarush/smia/licensing_service/model/License.java
+++ licensing-service/src/main/java/dev/javarush/smia/licensing_service/model/License.java
@@ -5,18 +5,43 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
 @Table(name = "licenses")
-public record License(
-        @Id @Column(name = "license_id", nullable = false) String licenseId,
-        String description,
-        @Column(name = "organization_id", nullable = false) String organizationId,
-        @Column(name = "product_name", nullable = false) String productName,
-        @Column(name = "license_type", nullable = false) String licenseType,
-        @Column(name = "comment") String comment
-) {
+public final class License {
+    @Id
+    @Column(name = "license_id", nullable = false)
+    private String licenseId;
+    private String description;
+    @Column(name = "organization_id", nullable = false)
+    private String organizationId;
+    @Column(name = "product_name", nullable = false)
+    private String productName;
+    @Column(name = "license_type", nullable = false)
+    private String licenseType;
+    @Column(name = "comment")
+    private String comment;
+
+    public License(
+            String licenseId,
+            String description,
+            String organizationId,
+            String productName,
+            String licenseType,
+            String comment
+    ) {
+        this.licenseId = licenseId;
+        this.description = description;
+        this.organizationId = organizationId;
+        this.productName = productName;
+        this.licenseType = licenseType;
+        this.comment = comment;
+    }
+
+    public License() {
+    }
 
     public License withOrganizationId(String orgId) {
         return new License(
@@ -50,4 +75,58 @@ public record License(
                 comment
         );
     }
+
+    public String getLicenseId() {
+        return licenseId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getOrganizationId() {
+        return organizationId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getLicenseType() {
+        return licenseType;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (License) obj;
+        return Objects.equals(this.licenseId, that.licenseId) &&
+                Objects.equals(this.description, that.description) &&
+                Objects.equals(this.organizationId, that.organizationId) &&
+                Objects.equals(this.productName, that.productName) &&
+                Objects.equals(this.licenseType, that.licenseType) &&
+                Objects.equals(this.comment, that.comment);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(licenseId, description, organizationId, productName, licenseType, comment);
+    }
+
+    @Override
+    public String toString() {
+        return "License[" +
+                "licenseId=" + licenseId + ", " +
+                "description=" + description + ", " +
+                "organizationId=" + organizationId + ", " +
+                "productName=" + productName + ", " +
+                "licenseType=" + licenseType + ", " +
+                "comment=" + comment + ']';
+    }
+
 }
